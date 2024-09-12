5d34b1bb48852b358eb84bbb23383196438002d1
betül aktoprak <betulaktoprak112@gmail.com>
2024-09-13 00:55:46 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,72 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [FormsModule, CommonModule],
+  styleUrl: "./app.component.css",
+  template: `
+  <div class="container">
+    <h1>Todo App</h1>
+
+    <div *ngIf="!isUpdateWorkFormActive" class="input-section">
+      <label for="work">Work</label>
+      <input id="work" [(ngModel)]="work" class="input-box">
+      <button (click)="save()" class="btn save-btn">Save</button>
+    </div>
+
+    <div *ngIf="isUpdateWorkFormActive" class="input-section">
+      <label for="updateWork">Update Work</label>
+      <input id="updateWork" [(ngModel)]="updateWork" class="input-box">
+      <button (click)="update()" class="btn update-btn">Update</button>
+    </div>
+
+    <hr>
+
+    <div class="list-section">
+      <ul>
+        
+          @for(data of todos; track data){
+            <li>
+            {{ data }}
+            @if (!isUpdateWorkFormActive) {
+            <button *ngIf="!isUpdateWorkFormActive" (click)="get($index)" class="btn update-btn">Update</button>
+          <button *ngIf="!isUpdateWorkFormActive" (click)="delete($index)" class="btn delete-btn">Delete</button>
+          }
+          </li>
+          
+        }
+      </ul>
+    </div>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'todo-app';
+  work:string = '';
+  updateWork:string = '';
+  updateIndex:number = 0;
+  todos:string[] = [];
+  isUpdateWorkFormActive:boolean = false;
+
+  save(){
+    this.todos.push(this.work);
+    this.work = '';
+  }
+
+  delete(index: number){
+    this.todos.splice(index, 1);
+  }
+
+  get(index: number){
+    this.updateIndex = index;
+    this.updateWork = this.todos[index];
+    this.isUpdateWorkFormActive = true;
+  }
+
+  update(){
+    this.todos[this.updateIndex] = this.updateWork;
+    this.isUpdateWorkFormActive = false;
+  }
 }
